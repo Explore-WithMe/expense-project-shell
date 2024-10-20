4f0af930c58f0865dc2205a09fc8efae0dc6b0b8
Naveen <hi.to.cloud@gmail.com>
2024-10-20 15:05:05 +0530

    Added Shell

--- mysql-idempotent.sh
+++ mysql-idempotent.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # check id of user ..> id ..> for root user id=0 always
 ID=$(id -u)
+
+if [ $ID -ne 0 ]; 
+    then
+        echo "Please run this script as super user."
+        exit 1
+    else
+        echo "You are super user."
+fi
+
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
@@ -12,14 +21,6 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-
-if [ $ID -ne 0 ]; 
-    then
-        echo "Please run this script as super user."
-        exit 1
-    else
-        echo "You are super user."
-fi
 
 VALIDATE(){
    if [ $1 -ne 0 ]
